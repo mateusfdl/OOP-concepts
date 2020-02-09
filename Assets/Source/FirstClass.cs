2221c6039be4daf16abdc53a29d47cde4a19fe7c
Matheus Lima <matheus.limastack@outlook.com>
2020-02-09 04:16:25 -0300

    Add sketch of project

---
+++ Assets/Source/FirstClass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Reference of this instance
+        FirstClass reference = this;
+
+        Debug.Log(reference.gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
